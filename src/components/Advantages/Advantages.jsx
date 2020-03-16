50403144f5612682c51fbe5e15f116946c40f855
Marlena Czerwińska <marlena.stef@gmail.com>
2020-03-16 21:31:47 +0100

    Add proptypes for components

--- src/components/Advantages/Advantages.jsx
+++ src/components/Advantages/Advantages.jsx
@@ -10,21 +10,21 @@ import Section from '../Section/Section';
 import styles from './Advantages.module.scss';
 
 function icons(icon) {
-  if (icon === 'faBrain') { return (<FontAwesomeIcon icon={faBrain} className={styles.icon} />); }
-  if (icon === 'faBolt') { return (<FontAwesomeIcon icon={faBolt} className={styles.icon} />); }
-  if (icon === 'faPuzzlePiece') { return (<FontAwesomeIcon icon={faPuzzlePiece} className={styles.icon} />); }
-  if (icon === 'faFootballBall') { return (<FontAwesomeIcon icon={faFootballBall} className={styles.icon} />); }
+  if (icon === 'faBrain') { icon = faBrain; }
+  if (icon === 'faBolt') { icon = faBolt; }
+  if (icon === 'faPuzzlePiece') { icon = faPuzzlePiece; }
+  if (icon === 'faFootballBall') { icon = faFootballBall; }
+  return (icon);
 }
 
-const Advantages = ({title, subtitle, advantage}) => (
+const Advantages = ({ title, subtitle, advantage }) => (
   <Section title={title} subtitle={subtitle} variant="left_dark">
     <div className={styles.content}>
       <div className={styles.advantages_wrapper}>
         
         {advantage.map(item => (
           <div key={item.title} className={styles.advantage + item.class.split(' ').map(name => ' ' + (styles[name] || name)).join('')}>
-            {console.log(item.icon)}
-            {icons(item.icon)}
+            <FontAwesomeIcon icon={icons(item.icon)} className={styles.icon} />
             <h3 className={styles.title}>{item.title}</h3>
             <p className={styles.text}>{item.text}</p>
           </div>
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,5 +1,5 @@
-/* eslint-disable prefer-destructuring */
-/* eslint-disable react/destructuring-assignment */
+/* eslint-disable object-curly-newline */
+
 import React from 'react';
 import PropTypes from 'prop-types';
 
@@ -24,16 +24,16 @@ class App extends React.Component {
   }
 
   render() {
-    const language = this.state.language;
+    const { language: { id, header, aboutMe, portfolio, advantages, contact } } = this.state;
     return (
       <div>
-        {console.log(language)}
-        <Layout lang={language.id}>
-          <Header title={language.header.title} subtitle={language.header.subtitle} />
-          <AboutMe title={language.aboutMe.title} text={language.aboutMe.text} text2={language.aboutMe.text2} />
-          <Portfolio title={language.portfolio.title} subtitle={language.portfolio.subtitle} portfolio={language.portfolio.portfolio} />
-          <Advantages title={language.advantages.title} subtitle={language.advantages.subtitle} advantage={language.advantages.advantage} />
-          <Contact title={language.contact.title} name={language.contact.name} email={language.contact.email} message={language.contact.message} placeholder={language.contact.placeholder} button={language.contact.button} modal={language.contact.modal} />
+        {console.log(id, header, aboutMe, portfolio, advantages, contact)}
+        <Layout lang={id}>
+          <Header title={header.title} subtitle={header.subtitle} />
+          <AboutMe title={aboutMe.title} text={aboutMe.text} text2={aboutMe.text2} />
+          <Portfolio title={portfolio.title} subtitle={portfolio.subtitle} portfolio={portfolio.portfolio} />
+          <Advantages title={advantages.title} subtitle={advantages.subtitle} advantage={advantages.advantage} />
+          <Contact title={contact.title} name={contact.name} email={contact.email} message={contact.message} placeholder={contact.placeholder} button={contact.button} modal={contact.modal} />
         </Layout>
       </div>
     );
